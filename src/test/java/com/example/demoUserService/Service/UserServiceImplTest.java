80dfb7a9a004084214cac01b0353764881c625e5
Островский Леонид Иванович <69OstrovskiyLI@region.cbr.ru>
2020-01-22 15:17:13 +0700

    tests

--- src/test/java/com/example/demoUserService/Service/UserServiceImplTest.java
+++ src/test/java/com/example/demoUserService/Service/UserServiceImplTest.java
@@ -1,5 +1,9 @@
 package com.example.demoUserService.Service;
 
+import ch.qos.logback.core.util.FileUtil;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -12,48 +16,79 @@ import java.util.Date;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceImplTest {
-    Path path = Paths.get("D:\\Projects\\demoUserService\\src\\test\\resources\\tmp");
-    UserService userService = new UserServiceImpl(path);
+    Path path;
+    UserService userService;
 
-
-    @Test
-    void getSizeFolder() {
-        Path target = Paths.get(path.toString() + File.separator + "test.csv");
+    @BeforeEach
+    public void setUp() {
+        path = Paths.get("src\\test\\resources\\tmp");
+        userService = new UserServiceImpl(path);
+        Path newFolder = Paths.get(path.toString() + File.separator + "test1");
         try {
-            Path file = Files.createFile(target);
+            FileUtils.writeStringToFile(new File(newFolder.toString() + File.separator + "testData.txt"), "Это тест, и в нем тестируются тесты.", "utf-8");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        long expected = 0;
-        Date firstDate = new Date();
+
+    }
+
+    @Test
+    void getSizeFolder() {
+        Path newFolder2 = Paths.get(path.toString() + File.separator + "test2");
+        try {
+            FileUtils.writeStringToFile(new File(newFolder2.toString() + File.separator + "testData2.txt"), "Это второй тест.", "utf-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        long expected = 93;
         long actual = userService.getSizeFolder();
-        Date lastDate = new Date();
-        System.out.println("size folder - " + actual + "; time - " + (lastDate.getTime() - firstDate.getTime()));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void deleteFolder(){
+        String fileName = "test1";
+        userService.deleteFile(fileName);
+        Path folderPath = Paths.get(path.toString() + File.separator + fileName);
+        assertFalse(folderPath.toFile().exists());
     }
 
     @Test
     void deleteFile() {
-        String fileName = "test2";
-        Date firstDate = new Date();
+        String fileName = "test1\\testData.txt";
         userService.deleteFile(fileName);
-        Date lastDate = new Date();
-        System.out.println("delete file time " + (lastDate.getTime() - firstDate.getTime()));
+        File testFile = new File(path.toString() + File.separator + fileName);
+        assertFalse(testFile.exists());
     }
 
     @Test
     void deleteAll() {
-        Date firstDate = new Date();
+        Path newFolder2 = Paths.get(path.toString() + File.separator + "test2");
+        try {
+            FileUtils.writeStringToFile(new File(newFolder2.toString() + File.separator + "testData2.txt"), "Это второй тест.", "utf-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         userService.deleteAll();
-        Date lastDate = new Date();
-        System.out.println("delete all time " + (lastDate.getTime() - firstDate.getTime()));
+        assertFalse(path.toFile().exists());
     }
 
     @Test
     void zipFolder() {
-        String folder = "test3";
+        String folder = "test1";
         Date firstDate = new Date();
         userService.zipFolder(folder);
         Date lastDate = new Date();
+
         System.out.println("zip Folder time " + (lastDate.getTime() - firstDate.getTime()));
+    }
+
+    @AfterEach
+    public void clean() {
+        try {
+            FileUtils.deleteDirectory(path.toFile());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
